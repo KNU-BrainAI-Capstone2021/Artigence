5e5d3773997f2b7906c94d091b2280ac4768bc06
nowwhy <nowwhy@naver.com>
2021-06-02 16:15:47 +0900

    test

---
+++ JHJ/EEGNet/EEGNet_test.py
@@ -0,0 +1,221 @@
+import numpy as np
+
+# mne imports
+import mne
+#from mne import io
+#from mne.datasets import sample
+
+# EEGNet-specific imports
+from EEGNet_model import EEGNet
+from tensorflow.keras import utils as np_utils
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras import backend as K
+
+# PyRiemann imports
+#from pyriemann.estimation import XdawnCovariances
+#from pyriemann.tangentspace import TangentSpace
+#from pyriemann.utils.viz import plot_confusion_matrix
+#from sklearn.pipeline import make_pipeline
+#from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+# tools for plotting confusion matrices
+from matplotlib import pyplot as plt
+
+
+
+# tf.debugging.set_log_device_placement(True)
+
+# 텐서를 CPU에 할당
+
+# while the default tensorflow ordering is 'channels_last' we set it here
+# to be explicit in case if the user has changed the default ordering
+K.set_image_data_format('channels_last')
+
+##################### Process, filter and epoch the data ######################
+dpath = "D:/Artigence/data/s11/s11_pir.set"
+eeglab_raw  = mne.io.read_raw_eeglab(dpath)
+print(eeglab_raw.annotations[1])
+print(len(eeglab_raw.annotations))
+print(set(eeglab_raw.annotations.duration))
+print(set(eeglab_raw.annotations.description))
+print(eeglab_raw.annotations.onset[0])
+custom_mapping = {'left': 1, 'right': 2}
+(events_from_annot,event_dict) = mne.events_from_annotations(eeglab_raw, event_id=custom_mapping)
+print(event_dict)
+print(events_from_annot[:5])
+event_id = dict(left=1,right=2)
+tmin = 0
+tmax = 2.9980
+epochs = mne.Epochs(eeglab_raw, events_from_annot, event_id, tmin, tmax,baseline = None)
+labels = epochs.events[:,-1]
+
+X = epochs.get_data( )  # format is in (trials, channels, samples)
+X = np.hstack((X[:,8:25,:] , X[:,43:62,:]))
+Y = labels
+
+
+kernels, chans, samples = 1, 36, 1536
+
+# take 50/25/25 percent of the data to train/validate/test
+#X_train      = X[0:100,]
+#Y_train      = Y[0:100]
+#X_validate   = X[100:150,]
+#Y_validate   = Y[100:150]
+#X_test       = X[150:,]
+#Y_test       = Y[150:]
+
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y, train_size=0.75, shuffle=True,random_state=1004)
+print(X_train.shape)
+
+############################# EEGNet portion ##################################
+
+# convert labels to one-hot encodings.
+Y_train      = np_utils.to_categorical(Y_train -1)
+#Y_validate   = np_utils.to_categorical(Y_validate -1)
+Y_test       = np_utils.to_categorical(Y_test -1)
+# convert data to NHWC (trials, channels, samples, kernels) format. Data
+# contains 60 channels and 151 time-points. Set the number of kernels to 1.
+X_train      = X_train.reshape(X_train.shape[0], chans, samples, kernels)
+#X_validate   = X_validate.reshape(X_validate.shape[0], chans, samples, kernels)
+X_test       = X_test.reshape(X_test.shape[0], chans, samples, kernels)
+
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+kfold = KFold(n_splits=4, shuffle=True, random_state=0)
+
+# Chans, Samples  : number of channels and time points in the EEG data
+# configure the EEGNet-8,2,16 model with kernel length of 32 samples (other
+# model configurations may do better, but this is a good starting point)
+model = EEGNet(nb_classes = 2, Chans = chans, Samples = samples,
+               dropoutRate = 0.5, kernLength = 256, F1 = 4, D = 2, F2 = 8,
+               dropoutType = 'Dropout')
+
+# compile the model and set the optimizers
+model.compile(loss='categorical_crossentropy', optimizer='adam',
+              metrics = ['accuracy'])
+model.summary()
+# count number of parameters in the model
+numParams    = model.count_params()
+
+# set a valid path for your system to record model checkpoints
+checkpointer = ModelCheckpoint(filepath='D:/Artigence/tmp/checkpoint.h5', verbose=1,
+                               save_best_only=True)
+
+###############################################################################
+# if the classification task was imbalanced (significantly more trials in one
+# class versus the others) you can assign a weight to each class during
+# optimization to balance it out. This data is approximately balanced so we
+# don't need to do this, but is shown here for illustration/completeness.
+###############################################################################
+
+# the syntax is {class_1:weight_1, class_2:weight_2,...}. Here just setting
+# the weights all to be 1
+class_weights = {0 :1, 1 :1}
+
+################################################################################
+# fit the model. Due to very small sample sizes this can get
+# pretty noisy run-to-run, but most runs should be comparable to xDAWN +
+# Riemannian geometry classification (below)
+################################################################################
+hist = model.fit(X_train, Y_train, batch_size = 16, epochs = 100,
+                 verbose = 2, validation_split=0.33, shuffle=True,
+                 callbacks=[checkpointer], class_weight = class_weights)
+
+# load optimal weights
+model.load_weights('D:/Artigence/tmp/checkpoint.h5')
+
+###############################################################################
+# can alternatively used the weights provided in the repo. If so it should get
+# you 93% accuracy. Change the WEIGHTS_PATH variable to wherever it is on your
+# system.
+###############################################################################
+
+# WEIGHTS_PATH = /path/to/EEGNet-8-2-weights.h5
+# model.load_weights(WEIGHTS_PATH)
+
+###############################################################################
+# make prediction on test set.
+###############################################################################
+
+probs = model.predict(X_test)
+preds = probs.argmax(axis = -1)
+acc = np.mean(preds == Y_test.argmax(axis=-1))
+print("Classification accuracy: %f " % (acc))
+
+fig, loss_ax = plt.subplots()
+
+acc_ax = loss_ax.twinx()
+
+loss_ax.plot(hist.history['loss'], 'y', label='train loss')
+loss_ax.plot(hist.history['val_loss'], 'r', label='val loss')
+
+acc_ax.plot(hist.history['accuracy'], 'b', label='train acc')
+acc_ax.plot(hist.history['val_accuracy'], 'g', label='val acc')
+
+loss_ax.set_xlabel('epoch')
+loss_ax.set_ylabel('loss')
+acc_ax.set_ylabel('accuray')
+
+loss_ax.legend(loc='upper left')
+acc_ax.legend(loc='lower left')
+
+plt.show()
+
+#%%
+############################# PyRiemann Portion ##############################
+"""
+# code is taken from PyRiemann's ERP sample script, which is decoding in
+# the tangent space with a logistic regression
+
+n_components = 2  # pick some components
+
+# set up sklearn pipeline
+clf = make_pipeline(XdawnCovariances(n_components),
+                    TangentSpace(metric='riemann'),
+                    LogisticRegression())
+
+preds_rg     = np.zeros(len(Y_test))
+
+# reshape back to (trials, channels, samples)
+X_train      = X_train.reshape(X_train.shape[0], chans, samples)
+X_test       = X_test.reshape(X_test.shape[0], chans, samples)
+
+# train a classifier with xDAWN spatial filtering + Riemannian Geometry (RG)
+# labels need to be back in single-column format
+history = clf.fit(X_train, Y_train.argmax(axis = -1))
+preds_rg     = clf.predict(X_test)
+
+# Printing the results
+acc2 = np.mean(preds_rg == Y_test.argmax(axis = -1))
+print("Classification accuracy: %f " % (acc2))
+
+# plot the confusion matrices for both classifiers
+#names        = ['audio left', 'audio right', 'vis left', 'vis right']
+#plt.figure(0)
+#plot_confusion_matrix(preds, Y_test.argmax(axis = -1), names, title = 'EEGNet-8,2')
+
+#plt.figure(1)
+#plot_confusion_matrix(preds_rg, Y_test.argmax(axis = -1), names, title = 'xDAWN + RG')
+#fig, loss_ax = plt.subplots()
+
+acc_ax = loss_ax.twinx()
+
+loss_ax.plot(hist.history['loss'], 'y', label='train loss')
+loss_ax.plot(hist.history['val_loss'], 'r', label='val loss')
+
+acc_ax.plot(hist.history['accuracy'], 'b', label='train acc')
+acc_ax.plot(hist.history['val_accuracy'], 'g', label='val acc')
+
+loss_ax.set_xlabel('epoch')
+loss_ax.set_ylabel('loss')
+acc_ax.set_ylabel('accuray')
+
+loss_ax.legend(loc='upper left')
+acc_ax.legend(loc='lower left')
+
+plt.show()
+print("end")
+"""
