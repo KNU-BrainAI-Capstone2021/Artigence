78deb59b3e03537a9c7c442d979789652e7dabbb
Jaeseong PArk <wotjd0715@gmail.com>
2021-06-22 04:37:40 +0900

    cross perform

--- Cross_Subject.py
+++ Cross_Subject.py
@@ -6,7 +6,7 @@ source : https://github.com/vlawhern/arl-eegmodels
 import numpy as np
 import tensorflow as tf
 import mne
-import glob
+import random
 # EEGNet-specific imports
 from EEGNet import EEGNet
 from tensorflow.keras import utils as np_utils
@@ -33,36 +33,62 @@ kf = KFold(n_splits=4, shuffle= True, random_state= True)
 
 ##################### Process, filter and epoch the data ######################
 dpath = "C:/Users/PC/OneDrive - knu.ac.kr/Data/"
+#median = 56.75 good subject acc > medain else bad subject
+good_subject = [1,2,3,4,5,6,8,11,12,13,14,20,21,22,23,25,27,29,32,33,39,43,46,47,50,51]
+bad_subject = [7,9,10,15,16,17,18,19,24,26,28,30,31,34,35,36,37,38,40,41,42,44,45,48,49,52]
+
+event_id = dict(left=1, right=2)
+tmin = 0
+tmax = 2.9980
+custom_mapping = {'left': 1, 'right': 2}
 
 for test_num in range(1,53):
     X = []
     Y = []
-    for num in range(1,53):
-        if(num == test_num):
-            eeglab_raw = mne.io.read_raw_eeglab(dpath + "s" + str(num) + "_pir.set")
-            custom_mapping = {'left': 1, 'right': 2}
-            (events_from_annot, event_dict) = mne.events_from_annotations(eeglab_raw, event_id=custom_mapping)
-            event_id = dict(left=1, right=2)
-            tmin = 0
-            tmax = 2.9980
-            epochs = mne.Epochs(eeglab_raw, events_from_annot, event_id, tmin, tmax, baseline=None)
-            labels = epochs.events[:, -1]
-            data = epochs.get_data() * 1000  # format is in (trials, channels, samples)
-            X_test = np.array(data)
-            Y_test = np.array(labels)
+    X_train = []
+    Y_train = []
+    X_test = []
+    Y_test = []
+    random.shuffle(good_subject)
+    random.shuffle(bad_subject)
+    print(good_subject)
+    print(bad_subject)
+    good_count = 0
+    bad_count = 0
+    eeglab_raw = mne.io.read_raw_eeglab(dpath + "s" + str(test_num) + "_pir.set")
+    (events_from_annot, event_dict) = mne.events_from_annotations(eeglab_raw, event_id=custom_mapping)
+    epochs = mne.Epochs(eeglab_raw, events_from_annot, event_id, tmin, tmax, baseline=None)
+    labels = epochs.events[:, -1]
+    data = epochs.get_data() * 1000  # format is in (trials, channels, samples)
+    X_test = np.array(data)
+    Y_test = np.array(labels)
+    for good_num in good_subject:
+        if(good_num == test_num):
             continue
-        eeglab_raw  = mne.io.read_raw_eeglab(dpath + "s"+str(num)+"_pir.set")
-        custom_mapping = {'left': 1, 'right': 2}
+        eeglab_raw  = mne.io.read_raw_eeglab(dpath + "s"+str(good_num)+"_pir.set")
         (events_from_annot,event_dict) = mne.events_from_annotations(eeglab_raw, event_id=custom_mapping)
-        event_id = dict(left=1,right=2)
-        tmin = 0
-        tmax = 2.9980
         epochs = mne.Epochs(eeglab_raw, events_from_annot, event_id, tmin, tmax,baseline = None)
         labels = epochs.events[:,-1]
         data = epochs.get_data( ) *1000 # format is in (trials, channels, samples)
         X.extend(data)
         Y.extend(labels)
-    X_train=np.array(X)
+        good_count=good_count+1
+        if(good_count==4):
+            break;
+    for bad_num in bad_subject:
+        if(bad_num == test_num):
+            continue
+        eeglab_raw  = mne.io.read_raw_eeglab(dpath + "s"+str(bad_num)+"_pir.set")
+        (events_from_annot,event_dict) = mne.events_from_annotations(eeglab_raw, event_id=custom_mapping)
+        epochs = mne.Epochs(eeglab_raw, events_from_annot, event_id, tmin, tmax,baseline = None)
+        labels = epochs.events[:,-1]
+        data = epochs.get_data( ) *1000 # format is in (trials, channels, samples)
+        X.extend(data)
+        Y.extend(labels)
+        bad_count=bad_count+1
+        if(bad_count==4):
+            break
+    X_train = np.array(X)
     Y_train =np.array(Y)
 
     kernels, chans, samples = 1, 64, 1536
@@ -76,12 +102,13 @@ for test_num in range(1,53):
     # contains 60 channels and 151 time-points. Set the number of kernels to 1.
     X_train = X_train.reshape(X_train.shape[0], chans, samples, kernels)
     X_test = X_test.reshape(X_test.shape[0], chans, samples, kernels)
-
+    print(X_train.shape,"X train shape")
+    print(X_test.shape, "X test shape")
     # take 50/25/25 percent of the data to train/validate/test
-    X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, train_size=0.8, shuffle=True,
+    X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, train_size=0.9, shuffle=True,
                                                       random_state=1004)
 
-    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=20)
     # Chans, Samples  : number of channels and time points in the EEG data
     # kernelLength : half of data sampling rate
     model = EEGNet(nb_classes=2, Chans=chans, Samples=samples,
@@ -102,7 +129,7 @@ for test_num in range(1,53):
 
     ################################################################################
     # fit the model
-    ################################################################################
+    ################################################################################'
     hist = model.fit(X_train, Y_train, batch_size=16, epochs=150,
                      verbose=2, validation_data=(X_val, Y_val), shuffle=True,
                      class_weight=class_weights, callbacks=[early_stopping])
--- Within_Subject.py
+++ Within_Subject.py
@@ -32,115 +32,115 @@ K.set_image_data_format('channels_last')
 
 #set Kfold's k Num using n_splits
 kf = KFold(n_splits=4, shuffle= True, random_state= True)
+with tf.device('/GPU:0'):
+    ##################### Process Main  ######################
+    for i in range(17,34):
+        count = 1
+        #data path where preprocessed data
+        dpath = "C:/Users/PC/OneDrive - knu.ac.kr/Data/s"+str(i)+"_pir.set"
+        #get data and find event
+        eeglab_raw  = mne.io.read_raw_eeglab(dpath)
+        custom_mapping = {'left': 1, 'right': 2}
+        (events_from_annot,event_dict) = mne.events_from_annotations(eeglab_raw, event_id=custom_mapping)
+        event_id = dict(left=1,right=2)
+        tmin = 0
+        tmax = 2.9980
+        epochs = mne.Epochs(eeglab_raw, events_from_annot, event_id, tmin, tmax,baseline = None)
+        labels = epochs.events[:,-1]
+        #data *1000 mV to V
+        X = epochs.get_data( )*1000 # format is in (trials, channels, samples)
+        Y = labels
+        #samples = 3sec * 512Hz sampling rate
+        kernels, chans, samples = 1, 64, 1536
 
-##################### Process Main  ######################
-for i in range(1,31):
-    count = 1
-    #data path where preprocessed data
-    dpath = "C:/Users/PC/OneDrive - knu.ac.kr/Data/s"+str(i)+"_pir.set"
-    #get data and find event
-    eeglab_raw  = mne.io.read_raw_eeglab(dpath)
-    custom_mapping = {'left': 1, 'right': 2}
-    (events_from_annot,event_dict) = mne.events_from_annotations(eeglab_raw, event_id=custom_mapping)
-    event_id = dict(left=1,right=2)
-    tmin = 0
-    tmax = 2.9980
-    epochs = mne.Epochs(eeglab_raw, events_from_annot, event_id, tmin, tmax,baseline = None)
-    labels = epochs.events[:,-1]
-    #data *1000 mV to V
-    X = epochs.get_data( )*1000 # format is in (trials, channels, samples)
-    Y = labels
-    #samples = 3sec * 512Hz sampling rate
-    kernels, chans, samples = 1, 64, 1536
-
-    for train_index, test_index in kf.split(X):
-        X_train = X[train_index]
-        Y_train = Y[train_index]
-        print("x_train_shape", X_train.shape)
-        X_test = X[test_index]
-        Y_test = Y[test_index]
-
-
-        ############################# EEGNet portion ##################################
-        # convert labels to one-hot encodings.
-        Y_train      = np_utils.to_categorical(Y_train -1)
-        Y_test       = np_utils.to_categorical(Y_test -1)
-
-        # convert data to NHWC (trials, channels, samples, kernels) format. Data
-        # contains 60 channels and 151 time-points. Set the number of kernels to 1.
-        X_train      = X_train.reshape(X_train.shape[0], chans, samples, kernels)
-        X_test       = X_test.reshape(X_test.shape[0], chans, samples, kernels)
-
-        # take 50/25/25 percent of the data to train/validate/test
-        X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, train_size=0.67, shuffle=True,
-                                                          random_state=1004)
+        for train_index, test_index in kf.split(X):
+            X_train = X[train_index]
+            Y_train = Y[train_index]
+            print("x_train_shape", X_train.shape)
+            X_test = X[test_index]
+            Y_test = Y[test_index]
 
 
-        early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=100)
-        # Chans, Samples  : number of channels and time points in the EEG data
-        # kernelLength : half of data sampling rate
-        model = EEGNet(nb_classes = 2, Chans = chans, Samples = samples,
-                       dropoutRate = 0.8, kernLength = 256, F1 = 4, D = 2, F2 = 8,
-                       dropoutType = 'Dropout')
+            ############################# EEGNet portion ##################################
+            # convert labels to one-hot encodings.
+            Y_train      = np_utils.to_categorical(Y_train -1)
+            Y_test       = np_utils.to_categorical(Y_test -1)
 
-        # compile the model and set the optimizers using binary-cross entropy
-        model.compile(loss='binary_crossentropy', optimizer='adam',
-                      metrics = ['accuracy'])
-        model.summary()
-        # count number of parameters in the model
-        numParams    = model.count_params()
+            # convert data to NHWC (trials, channels, samples, kernels) format. Data
+            # contains 60 channels and 151 time-points. Set the number of kernels to 1.
+            X_train      = X_train.reshape(X_train.shape[0], chans, samples, kernels)
+            X_test       = X_test.reshape(X_test.shape[0], chans, samples, kernels)
 
-        ##############################################################################
-        # the syntax is {class_1:weight_1, class_2:weight_2,...}. Here just setting
-        # the weights all to be 1
-        class_weights = {0 :1, 1 :1}
+            # take 50/25/25 percent of the data to train/validate/test
+            X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, train_size=0.67, shuffle=True,
+                                                              random_state=1004)
 
-        ################################################################################
-        # fit the model
-        ################################################################################
-        hist = model.fit(X_train, Y_train, batch_size = 16, epochs = 150,
-                         verbose = 2, validation_data=(X_val,Y_val), shuffle=True,
-                         class_weight = class_weights, callbacks = [early_stopping])
 
-        ###############################################################################
-        # make prediction on test set.
-        ###############################################################################
+            early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=100)
+            # Chans, Samples  : number of channels and time points in the EEG data
+            # kernelLength : half of data sampling rate
+            model = EEGNet(nb_classes = 2, Chans = chans, Samples = samples,
+                           dropoutRate = 0.8, kernLength = 256, F1 = 4, D = 2, F2 = 8,
+                           dropoutType = 'Dropout')
 
-        probs = model.predict(X_test)
-        preds = probs.argmax(axis = -1)
-        acc = np.mean(preds == Y_test.argmax(axis=-1))
-        print("Classification accuracy: %f " % (acc))
+            # compile the model and set the optimizers using binary-cross entropy
+            model.compile(loss='binary_crossentropy', optimizer='adam',
+                          metrics = ['accuracy'])
+            model.summary()
+            # count number of parameters in the model
+            numParams    = model.count_params()
 
-        fig, loss_ax = plt.subplots()
+            ##############################################################################
+            # the syntax is {class_1:weight_1, class_2:weight_2,...}. Here just setting
+            # the weights all to be 1
+            class_weights = {0 :1, 1 :1}
 
-        acc_ax = loss_ax.twinx()
+            ################################################################################
+            # fit the model
+            ################################################################################
+            hist = model.fit(X_train, Y_train, batch_size = 16, epochs = 150,
+                             verbose = 2, validation_data=(X_val,Y_val), shuffle=True,
+                             class_weight = class_weights, callbacks = [early_stopping])
 
-        loss_ax.plot(hist.history['loss'], 'y', label='train loss')
-        loss_ax.plot(hist.history['val_loss'], 'r', label='val loss')
+            ###############################################################################
+            # make prediction on test set.
+            ###############################################################################
 
-        acc_ax.plot(hist.history['accuracy'], 'b', label='train acc')
-        acc_ax.plot(hist.history['val_accuracy'], 'g', label='val acc')
+            probs = model.predict(X_test)
+            preds = probs.argmax(axis = -1)
+            acc = np.mean(preds == Y_test.argmax(axis=-1))
+            print("Classification accuracy: %f " % (acc))
 
-        loss_ax.set_xlabel('epoch')
-        loss_ax.set_ylabel('loss')
-        acc_ax.set_ylabel('accuray')
+            fig, loss_ax = plt.subplots()
 
-        loss_ax.legend(loc='upper left')
-        acc_ax.legend(loc='lower left')
+            acc_ax = loss_ax.twinx()
 
-        f = open(save_dir, "a")
-        f.write("TEST"+ str(i) + "\n")
-        f.write("k =" + str(count) + "\n")
-        f.write("test_acc=" + str(hist.history['accuracy'][-1]) + "\n")
-        f.write("val_acc=" + str(hist.history['val_accuracy'][-1]) + "\n")
-        f.write("test_acc" + str(acc) + "\n")
-        f.close()
-        count = count+1
+            loss_ax.plot(hist.history['loss'], 'y', label='train loss')
+            loss_ax.plot(hist.history['val_loss'], 'r', label='val loss')
 
-        ##########################################
-        #show Confusion_matrix
-        ##########################################
-        names = ['hand left', 'hand right']
-        plt.figure()
-        plot_confusion_matrix(preds, Y_test.argmax(axis = -1), names, title = 'EEGNet-8,2')
-        #plt.show()
+            acc_ax.plot(hist.history['accuracy'], 'b', label='train acc')
+            acc_ax.plot(hist.history['val_accuracy'], 'g', label='val acc')
+
+            loss_ax.set_xlabel('epoch')
+            loss_ax.set_ylabel('loss')
+            acc_ax.set_ylabel('accuray')
+
+            loss_ax.legend(loc='upper left')
+            acc_ax.legend(loc='lower left')
+
+            f = open(save_dir, "a")
+            f.write("TEST"+ str(i) + "\n")
+            f.write("k =" + str(count) + "\n")
+            f.write("test_acc=" + str(hist.history['accuracy'][-1]) + "\n")
+            f.write("val_acc=" + str(hist.history['val_accuracy'][-1]) + "\n")
+            f.write("test_acc" + str(acc) + "\n")
+            f.close()
+            count = count+1
+
+            ##########################################
+            #show Confusion_matrix
+            ##########################################
+            names = ['hand left', 'hand right']
+            plt.figure()
+            plot_confusion_matrix(preds, Y_test.argmax(axis = -1), names, title = 'EEGNet-8,2')
+            #plt.show()
